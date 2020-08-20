a26d951bd9f3a0f405227e740116363ec607385e
xameeramir <zameeramir@gmail.com>
2020-08-20 15:36:08 +0530

    inital commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+describe('Login functionality', () => {
+  test('login form should accept email and password', () => {
+    // const { getByText } = render(<App />);
+    // const linkElement = getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+    // throw 'aaa'
+  });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  test('email & password fields should not be null', () => {
+    // const { getByText } = render(<App />);
+    // const linkElement = getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+    // throw 'aaa'
+  });
+
+  test('minimum length of password should be 8 chars', () => {
+    // const { getByText } = render(<App />);
+    // const linkElement = getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+    // throw 'aaa'
+  });
+
+  test('email id needs to be valid', () => {
+    // const { getByText } = render(<App />);
+    // const linkElement = getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+    // throw 'aaa'
+  });
+})
