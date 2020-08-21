160af80565ecc76c15bd100fb06c1b447247baa4
xameeramir <zameeramir@gmail.com>
2020-08-21 10:24:52 +0530

    initial commit

---
+++ src/Login/Login.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+import Login from './Login';
+
+// Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe('Login functionality', () => {
+    test('login form should accept email and password', () => {
+    });
+
+    test('email & password fields should not be null', () => {
+    });
+
+    test('minimum length of password should be 8 chars', () => {
+    });
+
+    test('email id needs to be valid', () => {
+    });
+
+    test('on signin, success message should be shown', () => {
+    });
+
+})
